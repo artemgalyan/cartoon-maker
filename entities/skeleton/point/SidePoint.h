2f3d10fd6dbdb837554844448054cc8fd93e8efe
Angelina-Varvashevich <90001649+Angelina-Varvashevich@users.noreply.github.com>
2022-07-20 16:32:49 +0300

    SidePoint implementation
    SidePoint.h and SidePoint.cpp

--- entities/skeleton/point/SidePoint.h
+++ entities/skeleton/point/SidePoint.h
@@ -2,9 +2,28 @@
 #define CARTOON_MAKER_LOGIC_SKELETON_POINT_SIDEPOINT_H_
 
 #include "point.h"
+#include "MainPoint.h"
+#include <QGraphicsLineItem>
 
 class SidePoint : public Point {
   // TODO: Create SidePoint class
+ public:
+  const static QColor BorderColor;
+  const static int BorderWidth = 3;
+  SidePoint(const double radius, double angle, MainPoint *parent);
+  double GetRadius() const;
+  double GetAngle() const;
+  void AddMouseMoveEventListener(std::function<void(SidePoint *)> function);
+ protected:
+  void mouseMoveEvent(QGraphicsSceneMouseEvent *event) override;
+ private slots:
+  void onPosChanged();
+ private:
+  QPen GetStyle() const override;
+  const double radius_;
+  double angle_;
+  QGraphicsLineItem *lineToParent_;
+  QVector<std::function<void(SidePoint *)>> listeners_;
 };
 
 #endif //CARTOON_MAKER_LOGIC_SKELETON_POINT_SIDEPOINT_H_
