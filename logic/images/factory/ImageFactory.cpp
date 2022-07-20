55354c92b86a5d129b687ff4b06bd0290995c22b
Angelina-Varvashevich <90001649+Angelina-Varvashevich@users.noreply.github.com>
2022-07-20 21:00:05 +0300

    ImageFactory implementation

--- logic/images/factory/ImageFactory.cpp
+++ logic/images/factory/ImageFactory.cpp
@@ -1,1 +1,14 @@
 #include "ImageFactory.h"
+
+ImageFactory *ImageFactory::instance_ = nullptr;
+
+QVector<Image *> ImageFactory::GetImagesByKey(QString key) const {
+  return images_.at(key);
+}
+
+ImageFactory &ImageFactory::Instance() {
+  if (instance_ == nullptr) {
+    instance_ = new ImageFactory(images_);
+  }
+  return *instance_;
+}
--- logic/images/factory/ImageFactory.h
+++ logic/images/factory/ImageFactory.h
@@ -1,8 +1,19 @@
 #ifndef CARTOON_MAKER_LOGIC_IMAGES_FACTORY_IMAGEFACTORY_H_
 #define CARTOON_MAKER_LOGIC_IMAGES_FACTORY_IMAGEFACTORY_H_
 
+#include <QString>
+#include <utility>
+#include "../Image.h"
+
 class ImageFactory {
-  // TODO: Crate ImageFactory class
+ public:
+  [[nodiscard]] QVector<Image *> GetImagesByKey(QString key) const;
+  ImageFactory &Instance();
+ private:
+  explicit ImageFactory(std::map<QString, QVector<Image *>> images) : images_(std::move(images)) {}
+  std::map<QString, QVector<Image *>> images_;
+  static ImageFactory *instance_;
+
 };
 
 #endif //CARTOON_MAKER_LOGIC_IMAGES_FACTORY_IMAGEFACTORY_H_
