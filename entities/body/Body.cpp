361b0005f9cab12f3cc0373bc16d36d8987ad707
Angelina-Varvashevich <90001649+Angelina-Varvashevich@users.noreply.github.com>
2022-07-20 18:34:18 +0300

    Partial Body implementation

--- entities/body/Body.cpp
+++ entities/body/Body.cpp
@@ -1,1 +1,17 @@
 #include "Body.h"
+QString Body::GetBodyType() {
+  return type_;
+}
+
+const Skeleton &Body::GetSkeleton() {
+  return skeleton_;
+}
+
+void Body::AddTo(QGraphicsScene *scene) const {
+  QVector<Point *> skeletonPoints = skeleton_.GetPoints(); //MainPoint is skeletonPoints[0]
+  scene->addItem(skeletonPoints[0]);
+}
+
+Body::Body(const Skeleton &skeleton, QVector<QImage *> images) : skeleton_(skeleton), images_(images) {
+
+}
--- entities/body/Body.h
+++ entities/body/Body.h
@@ -2,12 +2,20 @@
 #define CARTOON_MAKER_ENTITIES_BODY_BODY_H_
 
 #include "../skeleton/Skeleton.h"
+#include <QImage>
+#include <QGraphicsScene>
 
 class Body {
-  // TODO: Create Body class
-public:
-private:
-    Skeleton skeleton_;
+  // TODO: function Image <-> ID; -MakeBody() : void; fix constructor
+ public:
+  Body(const Skeleton &, QVector<QImage *>/*, function Image -> ID*/);
+  QString GetBodyType();
+  const Skeleton &GetSkeleton();
+  void AddTo(QGraphicsScene *scene) const;
+ private:
+  Skeleton skeleton_;
+  QVector<QImage *> images_;
+  const QString type_;
 };
 
 #endif //CARTOON_MAKER_ENTITIES_BODY_BODY_H_
