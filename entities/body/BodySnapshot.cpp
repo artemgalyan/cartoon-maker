f498d8d52c4895e99634b392887caf54269f0c8d
Angelina-Varvashevich <90001649+Angelina-Varvashevich@users.noreply.github.com>
2022-07-20 19:33:45 +0300

    BodySnapshot implementation

--- entities/body/BodySnapshot.cpp
+++ entities/body/BodySnapshot.cpp
@@ -1,1 +1,14 @@
 #include "BodySnapshot.h"
+
+#include <utility>
+
+SkeletonSnapshot BodySnapshot::GetSkeleton() const {
+  return skeleton_;
+}
+
+bool BodySnapshot::IsVisible() const {
+  return isVisible_;
+}
+
+BodySnapshot::BodySnapshot(SkeletonSnapshot skeleton, bool isVisible)
+    : skeleton_(std::move(skeleton)), isVisible_(isVisible) {}
--- entities/body/BodySnapshot.h
+++ entities/body/BodySnapshot.h
@@ -2,9 +2,16 @@
 #define CARTOON_MAKER_ENTITIES_BODY_BODYSNAPSHOT_H_
 
 #include "Body.h"
+#include "../skeleton/SkeletonSnapshot.h"
 
 class BodySnapshot {
-  // TODO: Create BodySnapshot class
+ public:
+  BodySnapshot(SkeletonSnapshot  skeleton, bool isVisible);
+  [[nodiscard]] SkeletonSnapshot GetSkeleton() const;
+  [[nodiscard]] bool IsVisible() const;
+ private:
+  SkeletonSnapshot skeleton_;
+  bool isVisible_ = true;
 };
 
 #endif //CARTOON_MAKER_ENTITIES_BODY_BODYSNAPSHOT_H_
