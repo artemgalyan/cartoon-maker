845b6e049f102f5d7480b007c2ab9c32d0fcf4b3
Angelina-Varvashevich <90001649+Angelina-Varvashevich@users.noreply.github.com>
2022-07-21 20:37:35 +0300

    partly implementation of BodyFactory

--- entities/body/factory/BodyFactory.cpp
+++ entities/body/factory/BodyFactory.cpp
@@ -1,1 +1,12 @@
 #include "BodyFactory.h"
+
+BodyFactory *BodyFactory::Instance() {
+  if (instance_ == nullptr) {
+    instance_ = new BodyFactory();
+  }
+  return instance_;
+}
+
+void BodyFactory::Initialize() {
+  instance_ = Instance();
+}
--- entities/body/factory/BodyFactory.h
+++ entities/body/factory/BodyFactory.h
@@ -1,8 +1,19 @@
 #ifndef CARTOON_MAKER_ENTITIES_BODY_FACTORY_BODYFACTORY_H_
 #define CARTOON_MAKER_ENTITIES_BODY_FACTORY_BODYFACTORY_H_
 
+#include "../Body.h"
+
 class BodyFactory {
   // TODO: Create BodyFactory class
+ public:
+  Body* CreateByType(const QString& type) const;
+  void Initialize();
+  static BodyFactory *Instance();
+ private:
+  void LoadModels();
+  BodyFactory() = default;
+  QVector<Body> models_;
+  static BodyFactory *instance_;
 };
 
 #endif //CARTOON_MAKER_ENTITIES_BODY_FACTORY_BODYFACTORY_H_
