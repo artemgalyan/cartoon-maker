17e67d2c6f3e6d28d99f0fade3023e0ed654dbc2
Angelina-Varvashevich <90001649+Angelina-Varvashevich@users.noreply.github.com>
2022-07-21 23:38:00 +0300

    BodyFactory implementation

--- entities/body/factory/BodyFactory.cpp
+++ entities/body/factory/BodyFactory.cpp
@@ -1,4 +1,5 @@
 #include "BodyFactory.h"
+#include "QFile"
 
 BodyFactory *BodyFactory::Instance() {
   if (instance_ == nullptr) {
@@ -9,4 +10,58 @@ BodyFactory *BodyFactory::Instance() {
 
 void BodyFactory::Initialize() {
   instance_ = Instance();
+}
+void BodyFactory::LoadModels() {
+  QFile inputFile("Snake/skeleton.txt");
+  if (inputFile.open(QIODevice::ReadOnly))
+  {
+    QTextStream in(&inputFile);
+    int lineBlock = in.readLine().toInt();
+    int coord1;
+    int coord2;
+    int parent;
+    QVector<Point*> points;
+    while (lineBlock!=0)
+    {
+      in>>coord1;
+      in>>coord2;
+      in>>parent;
+      if (parent==-1) points.push_back(new MainPoint(coord1, coord2));
+      else points.push_back(new SidePoint(coord1, coord2, points[parent]));
+      --lineBlock;
+    }
+    Skeleton skeleton(points);
+
+    lineBlock = in.readLine().toInt();
+    int pngNumber;
+    QString fileName;
+    QPointF offset;
+    int pointIndex;
+    QVector<Image*> images;
+    QHash<Image*, int> indexFromImage;
+    while (lineBlock!=0)
+    {
+      in>>pngNumber;
+      fileName = "Snake/" + QString::number(pngNumber) +".png";
+      in>>offset.rx();
+      in>>offset.ry();
+      in>>pointIndex;
+      QPixmap pixmap(fileName);
+      Image *image= new Image(pixmap,offset);
+      images.push_back(image);
+      indexFromImage.insert(image, pointIndex);
+      --lineBlock;
+    }
+    Body body(skeleton, images, indexFromImage, "Snake/");
+    models_.push_back(body);
+    inputFile.close();
+  }
+}
+Body *BodyFactory::CreateByType(QString &type) const {
+  int i;
+  for(i=0; i< models_.size(); i++){
+    if (models_[i].GetBodyType()==type)
+      return models_[i].Clone();
+  }
+  return nullptr;
 }
--- entities/body/factory/BodyFactory.h
+++ entities/body/factory/BodyFactory.h
@@ -2,12 +2,16 @@
 #define CARTOON_MAKER_ENTITIES_BODY_FACTORY_BODYFACTORY_H_
 
 #include "../Body.h"
+#include "../../skeleton/point/MainPoint.h"
+#include "../../skeleton/point/SidePoint.h"
+
+
 
 class BodyFactory {
   // TODO: Create BodyFactory class
  public:
-  Body* CreateByType(const QString& type) const;
-  void Initialize();
+  [[nodiscard]] Body* CreateByType(QString& type) const;
+  static void Initialize();
   static BodyFactory *Instance();
  private:
   void LoadModels();
