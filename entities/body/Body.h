3e4ea15a28dc6d5ec15ddbeaffb770664811f3eb
Angelina-Varvashevich <90001649+Angelina-Varvashevich@users.noreply.github.com>
2022-07-21 23:37:08 +0300

    QString type_ instead of  const QString type_

--- entities/body/Body.h
+++ entities/body/Body.h
@@ -7,18 +7,20 @@
 
 class Body {
  public:
-  explicit Body(const Skeleton&, QVector<Image *>, QHash<Image*, int>, QString);
+  explicit Body(const Skeleton &, QVector<Image *>, QHash<Image *, int>, QString);
+  //Body(const Body &)=default;
+  Body& operator=(const Body&) = default;
   [[nodiscard]] QString GetBodyType() const;
   [[nodiscard]] const Skeleton &GetSkeleton() const;
   void AddTo(QGraphicsScene *scene) const;
-  [[nodiscard]] Body* Clone() const;
+  [[nodiscard]] Body *Clone() const;
  private:
   static double ToDegrees(double rad);
   void ConnectImagesToPoints();
   Skeleton skeleton_;
-  QVector<Image*> images_;
-  QHash<Image*, int> indexFromImage_;
-  const QString type_;
+  QVector<Image *> images_;
+  QHash<Image *, int> indexFromImage_;
+  QString type_;
 };
 
 #endif //CARTOON_MAKER_ENTITIES_BODY_BODY_H_
