57a28c3d054ed819f335608fa00061a67930b6ea
Angelina-Varvashevich <90001649+Angelina-Varvashevich@users.noreply.github.com>
2022-07-22 19:40:46 +0300

    BodyFactory implementation

--- entities/body/factory/BodyFactory.cpp
+++ entities/body/factory/BodyFactory.cpp
@@ -1,7 +1,11 @@
 #include "BodyFactory.h"
 #include "QFile"
 
-BodyFactory * BodyFactory::instance_=nullptr;
+BodyFactory *BodyFactory::instance_ = nullptr;
+
+BodyFactory::BodyFactory() {
+  LoadModels();
+}
 
 BodyFactory *BodyFactory::Instance() {
   if (instance_ == nullptr) {
@@ -15,35 +19,39 @@ void BodyFactory::Initialize() {
 }
 
 void BodyFactory::LoadModels() {
- QString fileName ="res/models/Snake/skeleton.txt";
- LoadModelByType(fileName);
+  QDir models(":/res/models/");
+  models.setFilter(QDir::Dirs);
+  QDirIterator iterator(models, QDirIterator::Subdirectories);
+  while (iterator.hasNext()) {
+    QString subDirName = iterator.path();
+    LoadModelByType(subDirName);
+    iterator.next();
+  }
 }
 
-Body *BodyFactory::CreateByType(QString &type) const {
-  int i;
-  for(i=0; i< models_.size(); i++){
-    if (models_[i].GetBodyType()==type)
+Body *BodyFactory::CreateByType(const QString &type) const {
+  for (int i = 0; i < models_.size(); i++) {
+    if (models_[i].GetBodyType() == type)
       return models_[i].Clone();
   }
   return nullptr;
 }
 
-void BodyFactory::LoadModelByType(const QString &fileName) {
+void BodyFactory::LoadModelByType(const QString &subDirName) {
+  QString fileName = subDirName + "skeleton.txt";
   QFile inputFile(fileName);
-  if (inputFile.open(QIODevice::ReadOnly))
-  {
+  if (inputFile.open(QIODevice::ReadOnly)) {
     QTextStream in(&inputFile);
     int lineBlock = in.readLine().toInt();
     int coord1;
     int coord2;
     int parent;
-    QVector<Point*> points;
-    while (lineBlock!=0)
-    {
-      in>>coord1;
-      in>>coord2;
-      in>>parent;
-      if (parent==-1) points.push_back(new MainPoint(coord1, coord2));
+    QVector<Point *> points;
+    while (lineBlock != 0) {
+      in >> coord1;
+      in >> coord2;
+      in >> parent;
+      if (parent == -1) points.push_back(new MainPoint(coord1, coord2));
       else points.push_back(new SidePoint(coord1, coord2, points[parent]));
       --lineBlock;
     }
@@ -53,22 +61,22 @@ void BodyFactory::LoadModelByType(const QString &fileName) {
     QString pointFileName;
     QPointF offset;
     int pointIndex;
-    QVector<Image*> images;
-    QHash<Image*, int> indexFromImage;
-    while (lineBlock!=0)
-    {
-      in>>pointFileName;
-      in>>offset.rx();
-      in>>offset.ry();
-      in>>pointIndex;
+    QVector<Image *> images;
+    QHash<Image *, int> indexFromImage;
+    while (lineBlock != 0) {
+      in >> pointFileName;
+      in >> offset.rx();
+      in >> offset.ry();
+      in >> pointIndex;
       QPixmap pixmap(pointFileName);
-      Image *image= new Image(pixmap,offset);
+      Image *image = new Image(pixmap, offset);
       images.push_back(image);
       indexFromImage.insert(image, pointIndex);
       --lineBlock;
     }
-    Body body(skeleton, images, indexFromImage, fileName);
+    Body body(skeleton, images, indexFromImage, subDirName);
     models_.push_back(body);
     inputFile.close();
   }
 }
+
--- entities/body/factory/BodyFactory.h
+++ entities/body/factory/BodyFactory.h
@@ -1,22 +1,23 @@
 #ifndef CARTOON_MAKER_ENTITIES_BODY_FACTORY_BODYFACTORY_H_
 #define CARTOON_MAKER_ENTITIES_BODY_FACTORY_BODYFACTORY_H_
 
+#include <QDataStream>
+#include <QDir>
+#include <QDirIterator>
+#include <QTemporaryFile>
 #include "../Body.h"
 #include "../../skeleton/point/MainPoint.h"
 #include "../../skeleton/point/SidePoint.h"
 
-
-
 class BodyFactory {
-  // TODO: Create BodyFactory class
  public:
-  [[nodiscard]] Body* CreateByType(QString& type) const;
+  [[nodiscard]] Body *CreateByType(const QString &type) const;
   static void Initialize();
   static BodyFactory *Instance();
  private:
   void LoadModels();
-  void LoadModelByType(const QString& fileName);
-  BodyFactory() = default;
+  void LoadModelByType(const QString &subDirName);
+  BodyFactory();
   QVector<Body> models_;
   static BodyFactory *instance_;
 };
