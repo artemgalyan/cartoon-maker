389bc837420aecd16dcca0fe6ab419b77543c32d
artemgalyan <89996510+artemgalyan@users.noreply.github.com>
2022-07-22 20:56:59 +0300

    demo functionality

--- ui/testwidget.cpp
+++ ui/testwidget.cpp
@@ -1,12 +1,20 @@
 #include "testwidget.h"
 #include "ui_TestWidget.h"
 
+#include "../entities/body/factory/BodyFactory.h"
+
 TestWidget::TestWidget(QWidget *parent) :
     QWidget(parent), ui(new Ui::TestWidget) {
   ui->setupUi(this);
   view_ = ui->graphicsView;
+  view_->setRenderHint(QPainter::Antialiasing, true);
   scene_ = new QGraphicsScene();
   view_->setScene(scene_);
+  auto factory = BodyFactory::Instance();
+  auto snake = factory->CreateByType("Snake");
+  snake->AddTo(scene_);
+  auto pig = factory->CreateByType("Pig");
+  pig->AddTo(scene_);
 }
 
 TestWidget::~TestWidget() {
