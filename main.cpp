983f993d57696d63287d79e4595afc407045abe4
artemgalyan <89996510+artemgalyan@users.noreply.github.com>
2022-07-25 17:41:12 +0300

    switched from testwidget to CartoonEditor

--- main.cpp
+++ main.cpp
@@ -1,10 +1,13 @@
 #include <QApplication>
 
 #include "ui/testwidget.h"
+#include "entities/body/factory/BodyFactory.h"
+#include "ui/cartooneditor.h"
 
 int main(int argc, char *argv[]) {
   QApplication a(argc, argv);
-  TestWidget w;
-  w.show();
+  BodyFactory::Initialize();
+  CartoonEditor ce;
+  ce.showFullScreen();
   return QApplication::exec();
 }
