9abef8017cfae9cd9ef3cb657eba1a7c99cd5b21
Angelina-Varvashevich <90001649+Angelina-Varvashevich@users.noreply.github.com>
2022-08-03 17:29:27 +0300

    added tittle and icon

--- main.cpp
+++ main.cpp
@@ -4,11 +4,13 @@
 #include "ui/cartooneditor.h"
 #include "logic/factory/StyleManager.h"
 
-int main(int argc, char *argv[]) {
+int main(int argc, char* argv[]) {
   QApplication a(argc, argv);
   BodyFactory::Initialize();
   StyleManager::Initialize();
   CartoonEditor ce;
+  ce.setWindowTitle("Make your own cartoon!");
+  ce.setWindowIcon(QIcon(":res/icons/icon.png"));
   ce.show();
   return QApplication::exec();
 }
